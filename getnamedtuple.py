5123da5ba408630c0281daaec532434048ce7a23
Jim Witschey <jim.witschey@gmail.com>
2014-11-15 19:57:17 -0500

    documents new OrderedDict implementation

--- getnamedtuple.py
+++ getnamedtuple.py
@@ -13,34 +13,60 @@ def get_namedtuple(name, data=None, _verbose=False, _rename=False, **kw):
 
     becomes:
 
-    >>> s = get_namedtuple('Point', x=3, y=5)
-    >>> s.x, s.y
+    >>> p = get_namedtuple('Point', x=3, y=5)
+    >>> p.x, p.y
     (3, 5)
 
     This function can also take a dictionary as input:
 
     >>> g = {'informal': 'Hi there!', 'formal': 'Hello; nice to meet you.'}
-    >>> get_namedtuple('Greetings', g)
-    Greetings(formal='Hello; nice to meet you.', informal='Hi there!')
+    >>> greetings = get_namedtuple('Greetings', g)
+    >>> greetings.informal
+    'Hi there!'
+    >>> greetings.formal
+    'Hello; nice to meet you.'
 
     It raises a ValueError if passed both a dictionary and keyword arguments
     for namedtuple fields.
 
-    Note from the above example that the order of the fields is not preserved
-    from the order of the arguments. To access the fields by index, you must
-    first determine the index of the attribute you want by looking at the
-    _fields attribute:
+    Note that, in the above ways of calling get_namedtuple, the order of the
+    arguments to get_namedtuple is not necessarily preserved in the order of
+    the returned namedtuple's fields. In this case, to access particular
+    fields by index rather than by name, you must first determine the index of
+    the attribute you want by looking at the _fields attribute:
 
-    >>> i = s._fields.index('y')
-    >>> s[i]
-    5
+    >>> i = greetings._fields.index('informal')
+    >>> greetings[i]
+    'Hi there!'
 
-    If _verbose and/or _rename are true, it calls namedtuple with verbose and
-    rename respectively; these arguments begin with an underscore to allow
-    callers to specify fields called 'verbose' and 'rename'.
+    Do not write code that depends on the order of the returned namedtuple's
+    fields if you call the function in the above ways, as not all Python
+    implementations guarantee deterministic order for dictionary keys and
+    keyword arguments.
 
-    This function performs no error handling around the namedtuple call, and
-    may raise any error that namedtuple does.
+    You can also get namedtuples with guaranteed field order, sidestepping the
+    problem entirely, by passing an ordered collection of name-value pairs:
+
+    >>> get_namedtuple('Point', (('a', 5), ('b', 7)))._fields
+    ('a', 'b')
+
+    Internally, this is passed to the OrderedDict constructor, so inputs must
+    conform to valid OrderedDict constructor arguments. You can also construct
+    an OrderedDict yourself and pass it in if you prefer. See the
+    documentation for OrderedDict in the collections module for more
+    information.
+
+    If _verbose and/or _rename are true, it calls namedtuple with verbose
+    and/or rename, respectively; these arguments begin with an underscore to
+    allow callers to specify fields called 'verbose' and 'rename'.
+
+    This function performs no error handling around the instantiation of the
+    namedtuple class or object, so it may raise any error that namedtuple
+    does.
+
+    For further documentation on verbose, rename, errors, and the returned
+    namedtuple itself, see the documentation for namedtuple in the collections
+    module.
     '''
 
     if data is not None and kw != {}:
